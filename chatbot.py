735accdd22810576f73c8592c398869deae950c8
GINP07 <giovannipaduano2007@gmail.com>
2026-05-07 21:56:18 +0200

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -182,7 +182,7 @@ def generate_title(user, bot):
 # ================= SIDEBAR =================
 with st.sidebar:
     # Foto spostata SOPRA il pulsante Nuova Chat
-    st.image("img src="https://i.ibb.co/Xf5VVr4W/dani-munoz.png", use_container_width=True)
+st.image("https://i.ibb.co/Xf5VVr4W/dani-munoz.png", use_container_width=True)
     
     st.markdown("<br>", unsafe_allow_html=True)
     
